46c815938f0dfd6871eb372085d6f2263494b120
amazingrando <oest@amazingrando.com>
2023-10-29 17:04:02 -0400

    wip

--- src/pages/[...slug].js
+++ src/pages/[...slug].js
@@ -1,15 +1,62 @@
-import { GetStaticPathsResult, GetStaticPropsResult } from "next"
 import Head from "next/head"
-import { DrupalNode } from "next-drupal"
-
 import { drupal } from "lib/drupal"
-import { NodeArticle } from "assets/components/node--article"
 import { Layout } from "assets/components/layout"
+import { useState, useEffect } from "react"
+import linkIcon from '../../assets/images/icons/link-solid.svg'
 
-const RESOURCE_TYPES = ["node--article"]
+export default function NodePage({ resource, ruleBook }) {
+  const [toc, setToc] = useState([{}]);
+  
+  useEffect(() => {
+    const headers = document.querySelectorAll(
+      '.main-content-wrapper h2, .main-content-wrapper h3, .main-content-wrapper h4'
+    );
 
-export default function NodePage({ resource }) {
+    const processAnchorLinks = (header) => {
+      if (header.id !== '') {
+        const anchor = `<a href="#${header.id}" class="anchor" style="background-image: url(${linkIcon.src})"></a>`;
+        return (header.innerHTML += anchor);
+      }
+    };
+
+    headers.forEach((header) => {
+      processAnchorLinks(header);
+    });
+
+    const tocContent = () => {
+      const content = [];
+      for (const item of headers) {
+        if (item.tagName === 'H2') {
+          content.push({
+            text: item.textContent,
+            id: item.id,
+          });
+        }
+      }
+      return content;
+    };
+    setToc(tocContent());
+  }, []);
+
   if (!resource) return null
+  const book = ruleBook[0].name
+
+  function replacer(match, p1, p2, p3, offset, string) {
+    const hash = p2
+      .replace(/ /g, '-')
+      .replace(/[?,:()“”"'’+.*]/g, '')
+      .replace(/^-/, '')
+      .replace(/&amp;/, '')
+      .toLowerCase();
+    return `<h${p1} id="${hash}">${p2}</h${p1}>`;
+  }
+
+  let pageContent =
+  resource.body.processed !== ''
+  ? resource.body.processed
+  : resource.body.value;
+  
+  pageContent = pageContent.replace(/<h(\d+)>([^<>]*)<\/h(\d+)>/gi, replacer);
 
   return (
     <Layout>
@@ -17,7 +64,32 @@ export default function NodePage({ resource }) {
         <title>{resource.title}</title>
         <meta name="description" content="A Next.js site powered by Drupal." />
       </Head>
-      {resource.type === "node--article" && <NodeArticle node={resource} />}
+      {console.log(resource, 'resource')}
+      <main className="main-content-wrapper" role="main">
+        <p className="rules-section">{book}</p>
+        <h1 className="page-title">{resource.title}</h1>
+        {toc.length > 1 && (
+          <div className="toc">
+            <h5 className="toc__header">Table of Contents</h5>
+            <ul className="toc__ul">
+              {toc.map((item) => (
+                <li key={item.id}>
+                  <a href={`#${item.id}`}>{item.text}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        
+        <div
+          dangerouslySetInnerHTML={{ __html: pageContent }}
+          
+        />
+      
+      </main>
+      {/* <aside className="aside-wrapper">
+        <Aside ruleBook={ruleBook} authorlist={authorlist} />
+      </aside> */}
     </Layout>
   )
 }
@@ -33,7 +105,6 @@ export async function getStaticProps(
   context
 ) {
   const path = await drupal.translatePathFromContext(context)
-
   if (!path) {
     return {
       notFound: true,
@@ -73,9 +144,20 @@ export async function getStaticProps(
     }
   }
 
+  const ruleBook = await drupal.getResourceCollectionFromContext(
+    "taxonomy_term--fate_version",
+    context,
+    {
+      params: {
+        "filter[id]": resource.field_tags[0].id,
+      },
+    }
+  )
+
   return {
     props: {
       resource,
+      ruleBook
     },
   }
 }
