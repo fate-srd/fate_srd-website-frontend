5ca3e46019138266e916e4fbb4c288f7214ca053
amazingrando <oest@amazingrando.com>
2023-11-10 15:23:38 -0500

    wip

---
+++ src/pages/contact-us/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import { Layout } from '../../../assets/components/layout';
+
+export default function ContactUs() {
+  return (
+    <Layout>
+      <main className="main-content-wrapper">
+        <h1 className="page-title">Contact Us</h1>
+        <iframe
+          title="Contact Us"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfqNSI0EYTYqSwyNv1MJxowYxY0Th49XP6Ps7VESQyFbR6uyA/viewform?embedded=true"
+          width="640"
+          height="640"
+          frameBorder="0"
+          marginHeight="0"
+          marginWidth="0"
+        >
+          Loading…
+        </iframe>
+      </main>
+    </Layout>
+  );
+}
