274ebd8645ed64a669fee241bb123a2225ea827a
amazingrando <oest@amazingrando.com>
2023-11-19 20:13:26 -0500

    wip

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,5 @@
 import '../../assets/components/style.scss';
-import ErrorBoundary from '../../lib/errorBoundary';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <ErrorBoundary>
-      <Component {...pageProps} />;
-    </ErrorBoundary>
-  );
+  return <Component {...pageProps} />;
 }
