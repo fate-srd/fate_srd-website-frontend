f090f1f65b8a18b4174038c0f2aabc9fd1738a39
amazingrando <oest@amazingrando.com>
2023-11-21 17:12:19 -0500

    stunt maker

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: [process.env.NEXT_IMAGE_DOMAIN, 'i.ytimg.com', 'yt3.ggpht.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/stunt-maker',
+        destination: '/stuntmaker/index.html',
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
