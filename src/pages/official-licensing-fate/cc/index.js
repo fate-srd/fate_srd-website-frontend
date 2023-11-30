c4737dbb641ce9dfed779d5acb7011a1baba29d6
amazingrando <oest@amazingrando.com>
2023-11-29 21:21:24 -0500

    lint

--- src/pages/official-licensing-fate/cc/index.js
+++ src/pages/official-licensing-fate/cc/index.js
@@ -131,7 +131,8 @@ const Page = () => (
       </p>
       <blockquote className="formal">
         This work is based on Fate Core System and Fate Accelerated Edition
-        (found at <a href="https://www.faterpg.com/">https://www.faterpg.com/</a>
+        (found at{' '}
+        <a href="https://www.faterpg.com/">https://www.faterpg.com/</a>
         ), products of Evil Hat Productions, LLC, developed, authored, and
         edited by Leonard Balsera, Brian Engard, Jeremy Keller, Ryan Macklin,
         Mike Olson, Clark Valentine, Amanda Valentine, Fred Hicks, and Rob
