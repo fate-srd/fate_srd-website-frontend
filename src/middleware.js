62725be3c80e4dace34109714e87719c812f6bea
amazingrando <oest@amazingrando.com>
2023-12-05 18:02:18 -0500

    node fix

--- src/middleware.js
+++ src/middleware.js
@@ -1,33 +1,26 @@
 import { NextResponse } from 'next/server';
 
 export async function middleware(request) {
-  // console.log('request', typeof request, request);
-  console.log('START');
+  let destinationURL = '/';
   const splitPath = request.nextUrl.pathname.split('/');
 
-  const newURL = () => {
-    console.log('fetching');
-    fetch(
+  const getURL = async () => {
+    const nodeData = await fetch(
       `https://fatesrd.amazingrando.dev/jsonapi/node/article?filter%5Bdrupal_internal__nid%5D=${splitPath[2]}`,
       {
         method: 'GET',
       }
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        console.log('fetch worked');
-        console.log('json', json.data.length);
-        const destinationURL = json.data[0].attributes.path.alias
-          ? request.nextUrl.origin + json.data[0].attributes.path.alias
-          : '/';
-        console.log('destinationURL', destinationURL);
-        return NextResponse.redirect(new URL(destinationURL, request.url));
-      });
+    );
+    const data = await nodeData.json();
+    return data;
   };
 
   if (splitPath[1] === 'node') {
-    newURL();
+    const temp = await getURL();
+    destinationURL = temp.data[0].attributes.path.alias;
   }
+
+  return NextResponse.redirect(new URL(destinationURL, request.url));
 }
 
 export const config = {
